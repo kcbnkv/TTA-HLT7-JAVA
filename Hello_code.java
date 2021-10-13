a4206704745a2002344c2d1ef8bb041282a21a34
Karina Cabanikova <87716339+kcbnkv@users.noreply.github.com>
2021-10-13 20:24:41 +0100

    Update and rename Main_test.java to Hello_code.java

---
+++ Hello_code.java
@@ -0,0 +1,11 @@
+public class Main_test {
+    public static void sayHello()
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi im main method");
+        sayHello();
+    }
+}
+
