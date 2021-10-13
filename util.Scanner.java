dacf1459adbe83773d63f8a2ae7617f28900ad95
Karina Cabanikova <87716339+kcbnkv@users.noreply.github.com>
2021-10-13 20:38:08 +0100

    Update and rename Main_test.java to util.Scanner.java

---
+++ util.Scanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main_test {
+    public static void main(String[] args)
+    {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first no= ");
+
+int num1, num2;
+
+num1=sc.nextInt();
+System.out.println("Enter 2nd no= ");
+
+num2=sc.nextInt();
+System.out.println("Sum of no is= "+(num1+num2));
+}
+}
+
+
+
+
